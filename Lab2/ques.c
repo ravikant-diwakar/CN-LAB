dc30679a4338f2339abe0d193648d17261192f1b
RAVIKANT DIWAKAR <110620635+ravikant-diwakar@users.noreply.github.com>
2023-08-12 00:44:39 +0530

    ques.c

---
+++ Lab2/ques.c
@@ -0,0 +1,4 @@
+Q1. Write a program in c to display the memory address and content of a given value of the variable.
+Q2. Write a program to find the endianess of a given user value.
+Q3.	Write a program in c to convert the endianess of a little endian value to a big endian value and vice-versa.
+Q4. Write a c program to check whether the host machine is in little endian or in a big endian. Enter a number,print the content of each bit location and convert the endianess of the same.
